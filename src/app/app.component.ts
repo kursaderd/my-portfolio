091559952402fb62e9c0195e159089ecb93d5757
Kürşad Erdoğan <Krad Erdoan@DESKTOP-1BN8ROB>
2025-12-03 01:40:56 +0300

    Portfolio tamamlandi: Hero, Experience ve Skills eklendi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeroComponent } from './features/hero/hero.component';
+import { AboutComponent } from './features/about/about.component';
+import { ExperienceComponent } from './features/experience/experience.component';
+import { SkillsComponent } from './features/skills/skills.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,HeroComponent,AboutComponent,ExperienceComponent,SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
